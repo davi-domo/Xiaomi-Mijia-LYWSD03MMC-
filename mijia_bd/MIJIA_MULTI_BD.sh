208d8f86aacb94ebb92f857e3c55d7196daea00e
DAVID <d.auvre@free.fr>
2021-01-24 00:01:41 +0100

    Update MIJIA_MULTI_BD.sh

--- mijia_bd/MIJIA_MULTI_BD.sh
+++ mijia_bd/MIJIA_MULTI_BD.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#script d'interogation de module Xiaomi Mijia LYWSD03MMC
+#extraction de la liste des modules via une base de donnée sqlite3
+#enregistrement des resultat dan la base de donnée
+#script inspirée du blog de Fanjoe's : https://www.fanjoe.be/?p=3911
+#https://smhosy.blogspot.com/
 #extraction du chemin
 dir=$(dirname $(readlink -f $0))
 
@@ -19,35 +24,46 @@ fi
 
 add_read()
 {
-   MACadd="$1"
-   LOCadd="$2"
+    #reception des variable de la fonction
+    MACadd="$1"
+    LOCadd="$2"
 
-   hciconfig hci0 down
-   hciconfig hci0 up
+    # activation du dongle bluetooth
+    hciconfig hci0 down
+    hciconfig hci0 up
 
-   hnd38=$(timeout 15 gatttool -b $MACadd --char-write-req --handle='0x0038' --value="0100" --listen | grep --max-count=1 "Notification handle")
+    # lecture de la Notification handle
+    hnd38=$(timeout 15 gatttool -b $MACadd --char-write-req --handle='0x0038'\
+     --value="0100" --listen | grep --max-count=1 "Notification handle")
 
-   if !([ -z "$hnd38" ])
-   then
-      temperature=${hnd38:39:2}${hnd38:36:2}
-      temperature=$((16#$temperature))
-      if [ "$temperature" -gt "10000" ];
-      then
-           temperature=$((-65536 + $temperature))
-      fi
-      temperature=$(echo "scale=1;$temperature/100" | bc)
+    if !([ -z "$hnd38" ])
+    then
+        #extraction de la valeur de temperature
+        temperature=${hnd38:39:2}${hnd38:36:2}
+        temperature=$((16#$temperature))
+        if [ "$temperature" -gt "10000" ];
+        then
+            temperature=$((-65536 + $temperature))
+        fi
+        temperature=$(echo "scale=1;$temperature/100" | bc)
 
-      humidity=${hnd38:42:2}
-      humidity=$((16#$humidity))
+        #extraction de la valeur de d'humidité
+        humidity=${hnd38:42:2}
+        humidity=$((16#$humidity))
 
-      hnd1b=$(gatttool --device=$MACadd --char-read -a 0x1b)
-      # Characteristic value/descriptor: 63
-      battery=${hnd1b:33:2}
-      battery=$((16#$battery))
-      #texte pour le fichier de log
-      echo "$(date +"%d/%m/%y %H:%M:%S") - [SUCCES] [$LOCadd]"
+        #lecture et extraction de la valeur de d'humidité
+        hnd1b=$(gatttool --device=$MACadd --char-read -a 0x1b)
+        # Characteristic value/descriptor: 63
+        battery=${hnd1b:33:2}
+        battery=$((16#$battery))
+
+        #texte pour le fichier de log
+        echo "$(date +"%d/%m/%y %H:%M:%S") - [SUCCES] [$LOCadd]"
         
-      sqlite3 $sqlite_bd "insert into ETAT_MODULE (MAC,LOC,TH,HD,BAT,TIME) values (\"$MACadd\",\"$LOCadd\",\"$temperature\",\"$humidity\",\"$battery\",\"$(date +%s)\")"
+        #insertion des données dans la table ETAT_MODULE
+        sqlite3 $sqlite_bd "insert into ETAT_MODULE (MAC,LOC,TH,HD,BAT,TIME)\
+        values (\"$MACadd\",\"$LOCadd\",\"$temperature\",\"$humidity\",\"$battery\"\
+        ,\"$(date +%s)\")"
      
    else
       #texte pour le fichier de log
@@ -55,11 +71,13 @@ add_read()
    fi
 
 }
+
 # Boucle de la bd MODULE pour lire les valeur
 sqlite3 $sqlite_bd "select id from MODULE" | while read prkey; do
    mac=$(sqlite3 $sqlite_bd "select MAC from MODULE where ID=$prkey")
    loc=$(sqlite3 $sqlite_bd "select LOC from MODULE where ID=$prkey")
    #on lance la fonction de lecture
    add_read $mac $loc
-   sleep 2
+   #On n'est pas des bourrins on laisse une petite pause-café
+   sleep 1
 done
